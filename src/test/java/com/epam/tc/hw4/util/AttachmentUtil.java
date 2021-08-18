a884b06c693d20ef5de0fa3e92a7882118ff8c98
Dmitrii Smertin <dmitrii_smertin@epam.com>
2021-08-18 13:49:41 +0300

    attachmentUtil fix

--- src/test/java/com/epam/tc/hw4/util/AttachmentUtil.java
+++ src/test/java/com/epam/tc/hw4/util/AttachmentUtil.java
@@ -13,9 +13,4 @@ public class AttachmentUtil {
         Allure.addAttachment(name, inputStream);
     }
 
-    @Attachment(type = "image/png", value = "try to use param {name}")
-    public byte[] makeScreenshotAttachment(final String name, final byte[] source) {
-        return source;
-    }
-
 }
