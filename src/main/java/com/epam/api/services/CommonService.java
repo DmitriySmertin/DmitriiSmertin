e7bd1430c2f8b608268c160d2334a485d82f5a45
Dmitriy Smertin <d_smertin@wargaming.net>
2021-09-15 13:41:40 +0300

    delete garbage

--- src/main/java/com/epam/api/services/CommonService.java
+++
@@ -1,7 +0,0 @@
-package com.epam.api.services;
-
-public class CommonService {
-
-    private PropertyService prop = new PropertyService();
-
-}
--- src/main/java/com/epam/api/services/URI.java
+++
@@ -1,7 +0,0 @@
-package com.epam.api.services;
-
-public class URI {
-
-    final static String GET_ALL_BOARDS_URI = "/1/members/me/boards/";
-
-}
